7378b96ea546a5607b077a606e85effbf4ee72b1
lucas <d2194499406@gmail.com>
2017-06-19 11:52:18 +0800

    implement simple logs CRUD

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,51 @@
 class LogsController < ApplicationController
 
   def index
-    @log = Log.all
+    @logs = Log.all
   end
-  
+
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+
+    if @log.save
+      redirect_to logs_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @log = Log.find(params[:id])
+  end
+
+  def edit
+    @log = Log.find(params[:id])
+  end
+
+  def update
+    @log = Log.find(params[:id])
+
+    if @log.update(log_params)
+      redirect_to logs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @log = Log.find(params[:id])
+    @log.destroy
+    redirect_to logs_path, notice: "Deleted!"
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:title, :body)
+  end
+
 end
